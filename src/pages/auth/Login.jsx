9e3012c31ede8069b43791acdf723583da86368c
Eisha373 <f2023065346@umt.edu.pk>
2026-08-10 23:48:27 +0500

    updated login page to make it link to the home page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,19 +1,28 @@
 import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import "../../auth.css";
 
 export function Login(){
+    const navigate=useNavigate();
+
+    function handleLogin(e){
+     e.preventDefault();
+     navigate("/");
+    }
     return(
         <div className="auth-container">
             <div className="auth-card">
          <img src="/images/burger-logo(1).jpg" alt="Bitezone Logo" className="auth-logo" />
             
             <h2>Welcome to Bitezone</h2>
-            <form>
+            <form onSubmit={handleLogin}>
                 <label htmlFor="email">Email:</label>
                 <input type="email" id="email" placeholder="Email" required/>
                 <label htmlFor="password">Password:</label>
                 <input type="password" id="password" placeholder="Password" required/>
+                
                 <button type="submit">Login</button>
+               
             </form>
             <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
         </div>
