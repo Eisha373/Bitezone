c58c45159f90afbebb117dcd7a8639e16e421b6f
Eisha373 <f2023065346@umt.edu.pk>
2026-08-10 11:06:29 +0500

    rename DummyProducts file, and changed its path in Home file

--- src/pages/customer/Home.jsx
+++ src/pages/customer/Home.jsx
@@ -1,4 +1,4 @@
-import { dummyProducts } from "../../data/dummyProducts";
+import { dummyProducts } from "../../data/DummyProducts";
 import { ProductCard } from "../../components/ProductCard";
 import { Navbar } from "../../components/Navbar";
 import {Footer} from "../../components/Footer";
