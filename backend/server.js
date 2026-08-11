2f512096b5bd186396486165ec415aea6b934eeb
Eisha373 <f2023065346@umt.edu.pk>
2026-08-11 19:01:57 +0500

    backend setup,install express.js,added server.js with necessary functions,connected mongodb with the backend server

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+ import dotenv from "dotenv";
+  import cors from "cors";
+  
+  dotenv.config(); 
+
+  mongoose.connect(process.env.MONGO_URI) 
+  .then(() => console.log("MongoDB connected")) 
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+  const app = express();
+   app.use(cors()); 
+   app.use(express.json()); 
+   app.get("/", (req, res) => { res.send("Bitezone backend is running"); });
+    const PORT = process.env.PORT || 5000; 
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
